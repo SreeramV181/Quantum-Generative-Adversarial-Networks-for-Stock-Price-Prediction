d18e68bd7cc08a80712fad20b98c2981255ad45b
Tiger Sun <tgsun@stanford.edu>
2019-06-06 00:41:50 -0700

    data parser + additional files

--- data/parseData.py
+++ data/parseData.py
@@ -1,3 +1,5 @@
+# deprecated
+
 import csv
 import random
 raw = []
---
+++ utils/parser.py
@@ -0,0 +1,51 @@
+import csv
+import random
+import math
+import numpy as np
+
+# returns training and testing datasets
+def parseCSV(filename, num_qubits = 8):
+	raw = []
+	buckets = 2**num_qubits
+
+	window_size = num_qubits // 2
+	with open(filename) as csv_data:
+		data = csv.reader(csv_data)
+		next(data)
+		for rows in data:
+			raw.append(float(rows[5]))
+	print(math.ceil(math.log(max(raw))/math.log(2)))
+	max_val = math.pow(2, math.ceil(math.log(max(raw))/math.log(2)))
+	print(max_val)
+	raw.reverse()
+	# window_size = 5
+	print(len(raw))
+	bucket_size = int(max_val / buckets)
+	print(bucket_size)
+	data_pairs = []
+	# print(raw)
+	raw_np = np.array(raw) // bucket_size
+	raw = list(raw_np)
+	for i in range(len(raw) - window_size):
+		data_pairs.append((raw[i:i+window_size], raw[i + window_size]))
+		print(data_pairs[i])
+
+	random.shuffle(data_pairs)
+
+	# print(data_pairs)
+
+	training_size = int(0.8 * len(raw))
+	testing_size = len(raw) - training_size
+
+	training = data_pairs[:training_size].copy()
+	testing = data_pairs[training_size:].copy()
+	return training, testing
+
+def main():
+	training, testing = parseCSV("data/daily_adjusted_FB.csv")
+	print(len(training))
+	print(len(testing))
+	print(len(training) + len(testing))
+
+if __name__ == '__main__':
+	main()

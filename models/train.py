06ef60c59aff1237dcecf92dcbe8b464e12e952d
Tiger Sun <tgsun@stanford.edu>
2019-06-06 01:31:49 -0700

    heeheeee

--- models/train.py
+++ models/train.py
@@ -1,5 +1,9 @@
 from models.qubits5 import *
 from utils.parser import *
+import pennylane as qml
+from pennylane import numpy as np
+from pennylane.optimize import *
+
 
 # print(gen_ansatz(0, 0))
 def main():
@@ -7,7 +11,35 @@ def main():
     training = parseCSV("data/daily_adjusted_FB.csv")
     num_epochs = 50
 
-    for i in range(num_epochs);
-        for input in training:
+    eps = 1e-2
+    gen_weights = np.random.normal(loc=np.pi, scale=eps, size=(NUM_QUBITS, NUM_LAYERS, PARAMS_PER_LAYER))
+    disc_weights = np.random.normal(loc=0.0, scale=eps, size=(NUM_FEATURES + 1, NUM_LAYERS, PARAMS_PER_LAYER))
+    opt = GradientDescentOptimizer(0.1)
+
+    print("Training the discriminator.")
+    for it in range(50):
+        disc_weights = opt.step(disc_cost, disc_weights)
+        cost = disc_cost(gen_weights, disc_weights)
+        if it % 5 == 0:
+            print("Step {}: cost = {}".format(it+1, cost))
+
+    print("Probability for the discriminator to classify real data correctly: ", prob_real_true(disc_weights))
+    print("Probability for the discriminator to classify fake data as real: ", prob_fake_true(gen_weights, disc_weights))
+
+    print("Training the generator.")
+
+    # train generator
+    for it in range(200):
+        gen_weights = opt.step(gen_cost, gen_weights)
+        cost = -gen_cost(gen_weights, disc_weights)
+        if it % 5 == 0:
+            print("Step {}: cost = {}".format(it, cost))
+
+    print("Probability for the discriminator to classify real data correctly: ", prob_real_true(disc_weights))
+    print("Probability for the discriminator to classify fake data as real: ", prob_fake_true(gen_weights, disc_weights))
+
+    # should be close to zero at joint optimum
+    print("Final cost function value: ", disc_cost(disc_weights))
+
 if __name__ == '__main__':
     main()

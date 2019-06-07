0ee1780104650afbfc95ca6978da520775e99d92
Sreeram Venkatarao <sreeram.v181@gmail.com>
2019-06-07 09:22:42 -0700

    Comment out discriminator and generator unnecessary functions

--- models/qubits5.py
+++ models/qubits5.py
@@ -34,7 +34,7 @@ def gen_ansatz(x, theta_g):
 #Defines the architecture used for the discriminator
 def disc_ansatz(x, theta_d):
     #Reshape theta so params are easier to access
-    #theta_d = theta_d.reshape(NUM_FEATURES + 1, NUM_LAYERS, PARAMS_PER_LAYER)
+    theta_d = theta_d.reshape(NUM_FEATURES + 1, NUM_LAYERS, PARAMS_PER_LAYER)
 
     for i in range(NUM_LAYERS):
         # Hadamard
@@ -50,46 +50,46 @@ def disc_ansatz(x, theta_d):
         for q in range(NUM_FEATURES):
             qml.CNOT(wires=[q, q + 1])
 
-def generator(x, theta_g):
-    """
-    Variational circuit meant to generate next stock price given 4 previous prices
-
-    Args:
-        x: array containing previous 4 stock prices
-        w: variables of the circuit to optimize
-    """
-
-    # hi
-
-    # for i in range(0, NUM_QUBITS - 2):
-    #     qml.CNOT(wires=[i, i + 1])
-    # for i in range(0, NUM_QUBITS - 1):
-    #     qml.Hadamard(wires=i)
-
-    # #Apply a layer of RX
-    # for i in range(0, NUM_QUBITS - 1):
-    #     qml.RX(w[i] * x[i], wires=i)
-
-    # initial_guess_theta = np.random.uniform(low=0, high=2 * np.pi, size=(NUM_QUBITS, NUM_LAYERS, 2))
-    gen_ansatz(x, theta_g)
-
-def discriminator(x, theta_d):
-    """
-    Variational circuit that predicts next stock price based on previous 4 stock prices
-
-    Args:
-        x: array containing previous 5 stock prices
-        w: variables of the circuit to optimize
-    """
-    #Entangle qubits,
-
-    # for i in range(0, NUM_QUBITS):
-    #     qml.Hadamard(wires=i)
-
-    # #Apply a layer of RX
-    # for i in range(0, NUM_QUBITS):
-    #     qml.RX(w[i] * x[i], wires=i)
-    disc_ansatz(x, theta_d)
+# def generator(x, theta_g):
+#     """
+#     Variational circuit meant to generate next stock price given 4 previous prices
+#
+#     Args:
+#         x: array containing previous 4 stock prices
+#         w: variables of the circuit to optimize
+#     """
+#
+#     # hi
+#
+#     # for i in range(0, NUM_QUBITS - 2):
+#     #     qml.CNOT(wires=[i, i + 1])
+#     # for i in range(0, NUM_QUBITS - 1):
+#     #     qml.Hadamard(wires=i)
+#
+#     # #Apply a layer of RX
+#     # for i in range(0, NUM_QUBITS - 1):
+#     #     qml.RX(w[i] * x[i], wires=i)
+#
+#     # initial_guess_theta = np.random.uniform(low=0, high=2 * np.pi, size=(NUM_QUBITS, NUM_LAYERS, 2))
+#     gen_ansatz(x, theta_g)
+#
+# def discriminator(x, theta_d):
+#     """
+#     Variational circuit that predicts next stock price based on previous 4 stock prices
+#
+#     Args:
+#         x: array containing previous 5 stock prices
+#         w: variables of the circuit to optimize
+#     """
+#     #Entangle qubits,
+#
+#     # for i in range(0, NUM_QUBITS):
+#     #     qml.Hadamard(wires=i)
+#
+#     # #Apply a layer of RX
+#     # for i in range(0, NUM_QUBITS):
+#     #     qml.RX(w[i] * x[i], wires=i)
+#     disc_ansatz(x, theta_d)
 
 
 @qml.qnode(dev)
@@ -98,7 +98,7 @@ def real_disc_circuit(data, disc_weights):
     Feeds discriminator with true examples
 
     """
-    discriminator(data, disc_weights)
+    disc_ansatz(data, disc_weights)
     return [qml.expval.Hadamard(i) for i in range(NUM_FEATURES + 1)]
 
 
@@ -107,7 +107,7 @@ def real_gen_circuit(data, gen_weights):
     """
     Feeds discriminator with true examples
     """
-    generator(data, gen_weights)
+    gen_ansatz(data, gen_weights)
     return [qml.expval.Hadamard(i) for i in range(NUM_QUBITS)]
 
 def gen_output(measurements):
